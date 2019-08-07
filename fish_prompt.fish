72aba46154be082e9173fe03af1569a80e9c883b
LastContinue <last@continue.com>
2019-08-07 15:52:04 -0500

    Fixing git information for orphan branches

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -6,7 +6,7 @@ function fish_prompt -d "snorin - oh-my-zsh sorin inspired prompt"
 	if git rev-parse ^ /dev/null
         git branch -qv | grep "\*" | grep -q detached
             and set -l current_branch (git rev-parse HEAD | cut -c1-7)
-            or set -l current_branch (git branch | sed -n '/\* /s///p')
+            or set -l current_branch (git symbolic-ref --short HEAD)
 
         printf (set_color blue)git(set_color brwhite):(set_color red)$current_branch' '
     end
