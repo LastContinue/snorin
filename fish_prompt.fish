d528ca260955aeb621ab27c0bcfd9fa1afbb76fd
LastContinue <LastContinue@users.noreply.github.com>
2020-10-02 10:41:26 -0500

    Fixed git information during rebase; added 'git:' prefix as optional via variable; snorin_random_chevrons no longer echos out colors - also sets a green chevron by default now

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -1,25 +1,27 @@
 function fish_prompt -d "Snorin - oh-my-zsh sorin inspired prompt"
-    # Main
+    #Main
 	printf (set_color cyan)(prompt_pwd)' '
 
-    #Determine if git repo is set...
+    #determine if git repo is set...
 	if git rev-parse ^ /dev/null
-        #still not 100% sure this is correct in case of a rebase conflict...
-        git branch -qv | grep "\*" | grep -q detached
-            and set -l current_branch (git rev-parse HEAD | cut -c1-7)
-            or set -l current_branch (git symbolic-ref --short HEAD)
-            or set -l current_branch '???'
-        printf (set_color blue)git(set_color brwhite):(set_color red)$current_branch' '
+        set -l git_status (git branch --show-current)
+        test -n "$git_status"; or set -l git_status (git rev-parse --short HEAD)
+
+        #optionally show "git:" in front of branch/revision
+        set -q snorin_show_git_prefix
+        and set -l git_prefix (set_color blue)git(set_color brwhite):
+        or set -l git_prefix ''
+
+        printf $git_prefix(set_color red)$git_status' '
     end
 
-    # print fun part of prompt
-    if test -n "$snorin_chevrons"
-        for chevron in $snorin_chevrons
-            printf (set_color $chevron)'❯'
-        end
-        printf ' '
-    else
-        printf (set_color green)'❯ '
+    #print fun part of prompt
+    set -l chevron '❯'
+    test -n "$snorin_chevrons"; or set -U snorin_chevrons green
+
+    for chevron_color in $snorin_chevrons
+        printf (set_color $chevron_color)$chevron
     end
+    printf ' '
     set_color normal
 end
