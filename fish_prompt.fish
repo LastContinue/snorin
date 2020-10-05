39818f397d97b031850430047b0abf5dbb4f5747
LastContinue <LastContinue@users.noreply.github.com>
2020-10-04 19:00:31 -0500

    fixing a bug for older versions of Git

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -2,17 +2,15 @@ function fish_prompt -d "Snorin - oh-my-zsh sorin inspired prompt"
     # where are we ?
 	printf (set_color cyan)(prompt_pwd)" "
 
-    # determine if git repo is set...
-	if git rev-parse ^ /dev/null
-        set git_status (git branch --show-current)
-        test -n "$git_status"; or set -l git_status (git rev-parse --short HEAD)
-
+    # determine if git repo is set and try to set some information 
+	if set git_info (git symbolic-ref --short HEAD ^ /dev/null) # try set branch name
+    or set git_info (git rev-parse --short HEAD ^ /dev/null) # else try to set hash
         # optionally show "git:" in front of branch/revision
         set -q snorin_show_git_prefix
         and set git_prefix (set_color blue)git(set_color brwhite):
         or set git_prefix ""
 
-        printf $git_prefix(set_color red)$git_status" "
+        printf $git_prefix(set_color red)$git_info" "
     end
 
     # print fun part of prompt
